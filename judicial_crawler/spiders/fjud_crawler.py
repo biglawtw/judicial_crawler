42f8b4712a8c9e22636347c640179f7be7ba8424
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2016-01-03 11:50:03 -0600

    Fixed list url referer and provide info of print url

--- judicial_crawler/spiders/fjud_crawler.py
+++ judicial_crawler/spiders/fjud_crawler.py
@@ -5,7 +5,7 @@ from scrapy.spiders import CrawlSpider
 from scrapy.http import Request
 
 entrance_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY01_1.aspx" # no need any referer
-list_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx" # need entrance url as referer
+list_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx" # need list url as referer
 detail_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx" # need list url as referer
 print_url = "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx" # need detail url as referer
 
@@ -13,7 +13,7 @@ print_url = "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx" # need detail 
 # "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx?id=201&v_court=TPS+%e6%9c%80%e9%ab%98%e6%b3%95%e9%99%a2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=11&searchkw=&jmain=&cw=0",
 # "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=2&searchkw=&jmain=&cw=0",
 
-# id: the id in the query result (required in detail)
+# id: 檢索結果編號 the judgement id of the query result (required in detail)
 # v_court: 法院名稱 the court name (required in list)
 # v_sys: 裁判類別 the law system (required in list)
 # jud_year: 判決字號 年份 the year of the judgement
@@ -29,17 +29,28 @@ print_url = "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx" # need detail 
 # jmain: 裁判主文 the main section search keyword
 # cw: unknown
 
+# "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx?jrecno=104%2c%E5%8F%B0%E6%8A%97%2c937%2c20151224&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jyear=104&jcase=%E5%8F%B0%E6%8A%97&jno=937&jdate=1041224&jcheck="
+
+# jrecno: 裁判字號 + 裁判日期, format: 104,台抗,937,20151224 (required in print)
+# v_court: same as above (required in print)
+# v_sys: same as above (required in print)
+# jyear: the year of the judgement (related to the html title)
+# jcase: 判決字號 常用字別 (related to the html title)
+# jno: 判決字號 第一欄位 (related to the html title)
+# jdate: 判決日期 the date of the judgement
+# jcheck: unknown
+
 class FJUD_Crawler(CrawlSpider):
     name = "FJUD_Crawler"
     allowed_domains = ["jirs.judicial.gov.tw"]
 
     def start_requests(self):
         start_urls = [
-            "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx?jrecno=104%2c%E5%8F%B0%E6%8A%97%2c937%2c20151224&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jyear=104&jcase=%E5%8F%B0%E6%8A%97&jno=937&jdate=1041224&jcheck="
+            "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=11&searchkw=&jmain=&cw=0"
         ]
         requests = []
         for item in start_urls:
-          requests.append(Request(url=item, headers={'Referer':detail_url}))
+          requests.append(Request(url=item, headers={'Referer':list_url}))
         return requests
 
     def parse(self, response):
