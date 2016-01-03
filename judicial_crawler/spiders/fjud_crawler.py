8fa85a5e70e5c9ac9d0af632a9153cd952c419a3
Wu Cheng-Han <jacky_cute0808@hotmail.com>
2016-01-03 12:22:13 -0600

    Updated fjud crawler to fake the referer base on the request url, added history info

--- judicial_crawler/spiders/fjud_crawler.py
+++ judicial_crawler/spiders/fjud_crawler.py
@@ -8,10 +8,13 @@ entrance_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY01_1.aspx" # no need any
 list_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx" # need list url as referer
 detail_url = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx" # need list url as referer
 print_url = "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx" # need detail url as referer
+history_url = "http://jirs.judicial.gov.tw/FJUD/HISTORYSELF.aspx" # no need any referer
 
-# "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx?id=1&v_court=TPS+%e6%9c%80%e9%ab%98%e6%b3%95%e9%99%a2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=1&searchkw=&jmain=&cw=0",
-# "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx?id=201&v_court=TPS+%e6%9c%80%e9%ab%98%e6%b3%95%e9%99%a2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=11&searchkw=&jmain=&cw=0",
-# "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=2&searchkw=&jmain=&cw=0",
+# 裁判書查詢 結果列表
+list_url_example = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=2&searchkw=&jmain=&cw=0"
+
+# 裁判書 詳細內容
+detail_url_example = "http://jirs.judicial.gov.tw/FJUD/FJUDQRY03_1.aspx?id=1&v_court=TPS+%e6%9c%80%e9%ab%98%e6%b3%95%e9%99%a2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=1&searchkw=&jmain=&cw=0"
 
 # id: 檢索結果編號 the judgement id of the query result (required in detail)
 # v_court: 法院名稱 the court name (required in list)
@@ -29,16 +32,27 @@ print_url = "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx" # need detail 
 # jmain: 裁判主文 the main section search keyword
 # cw: unknown
 
-# "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx?jrecno=104%2c%E5%8F%B0%E6%8A%97%2c937%2c20151224&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jyear=104&jcase=%E5%8F%B0%E6%8A%97&jno=937&jdate=1041224&jcheck="
+# 友善列印
+print_url_example = "http://jirs.judicial.gov.tw/FJUD/PrintFJUD03_0.aspx?jrecno=104%2c%E5%8F%B0%E6%8A%97%2c937%2c20151224&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jyear=104&jcase=%E5%8F%B0%E6%8A%97&jno=937&jdate=1041224&jcheck="
 
-# jrecno: 裁判字號 + 裁判日期, format: 104,台抗,937,20151224 (required in print)
-# v_court: same as above (required in print)
-# v_sys: same as above (required in print)
+# jrecno: 裁判字號 + 裁判日期, format: 104,台抗,937,20151224 (required)
+# v_court: same as above (required)
+# v_sys: same as above (required)
 # jyear: the year of the judgement (related to the html title)
 # jcase: 判決字號 常用字別 (related to the html title)
 # jno: 判決字號 第一欄位 (related to the html title)
 # jdate: 判決日期 the date of the judgement
 # jcheck: unknown
+
+# 歷審裁判
+history_url_example = "http://jirs.judicial.gov.tw/FJUD/HISTORYSELF.aspx?SwitchFrom=1&selectedOwner=H&selectedCrmyy=104&selectedCrmid=%E5%8F%B0%E9%9D%9E&selectedCrmno=000275&selectedCrtid=TPS"
+
+# SwitchFrom: unknown
+# selectedOwner: unknown
+# selectedCrmyy: the year of the judgement
+# selectedCrmid: 判決字號 常用字別
+# selectedCrmno: 判決字號 第一欄位
+# selectedCrtid: 法院名稱 英文簡寫
 
 class FJUD_Crawler(CrawlSpider):
     name = "FJUD_Crawler"
@@ -46,11 +60,21 @@ class FJUD_Crawler(CrawlSpider):
 
     def start_requests(self):
         start_urls = [
-            "http://jirs.judicial.gov.tw/FJUD/FJUDQRY02_1.aspx?&v_court=TPS+%E6%9C%80%E9%AB%98%E6%B3%95%E9%99%A2&v_sys=M&jud_year=&jud_case=&jud_no=&jud_no_end=&jud_title=&keyword=&sdate=20150101&edate=20160101&page=11&searchkw=&jmain=&cw=0"
+            entrance_url,
+            list_url_example,
+            detail_url_example,
+            print_url_example,
+            history_url_example
         ]
         requests = []
         for item in start_urls:
-          requests.append(Request(url=item, headers={'Referer':list_url}))
+            # fake the referer base on the request url
+            referer = ''
+            if item.startswith(list_url) or item.startswith(detail_url):
+                referer = list_url
+            elif item.startswith(print_url):
+                referer = detail_url
+            requests.append(Request(url=item, headers={'Referer':referer}))
         return requests
 
     def parse(self, response):
